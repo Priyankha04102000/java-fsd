9f3da66cc3ecce159b3c83ee5d4043462d8ac9c3
Priyankha04102000 <110082191+Priyankha04102000@users.noreply.github.com>
2022-08-16 11:46:34 +0530

    Add files via upload

---
+++ Phase 2--practice 2/ProductDetails.java
@@ -0,0 +1,24 @@
+public class ProductDetails {
+private int pid;
+private String pname;
+private int pcost;
+public int getPid() {
+	return pid;
+}
+public void setPid(int pid) {
+	this.pid = pid;
+}
+public String getPname() {
+	return pname;
+}
+public void setPname(String pname) {
+	this.pname = pname;
+}
+public int getPcost() {
+	return pcost;
+}
+public void setPcost(int pcost) {
+	this.pcost = pcost;
+}
+
+}
