599c9372046d9b3a8ee7c6aa418bac52a6ed6c0d
Priyankha04102000 <110082191+Priyankha04102000@users.noreply.github.com>
2022-09-05 14:08:22 +0530

    Add files via upload

---
+++ phase -3--practice-3-Handling-User-Authentication-master/HandlingUserAuthentication/src/main/java/com/UserAuthentication.java
@@ -0,0 +1,16 @@
+package com;
+
+public class UserAuthentication {
+	public String getUserName() {
+		return "naveen";
+	}
+	public String getEmail() {
+		return "naveen@gmail.com";
+	}
+	public int getPhnNum() {
+		return 123456780;
+	}
+	public String getPassword() {
+		return "naveen@123";
+	}
+}
